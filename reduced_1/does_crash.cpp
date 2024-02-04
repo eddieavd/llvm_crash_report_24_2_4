12e436977d4e5ec55b1a4a014774662f125d7782
Edhem Avdagic <edhemavdagic@gmail.com>
2024-02-04 19:52:02 +0100

    init

---
+++ reduced_1/does_crash.cpp
@@ -0,0 +1,30 @@
+class a
+{
+public:
+  using ssize_type = long ;
+
+  template< typename Self >
+  void at( this Self &&, ssize_type ) ;
+};
+
+template< typename > class b : a {} ;
+
+template< typename T = int >
+class c : b< T >
+{
+public:
+  using some_type = T ;
+
+  void erase() noexcept ;
+};
+
+template< typename T >
+void c< T >::erase() noexcept
+{
+  some_type f = at( f );
+}
+
+void h() {
+  c vec;
+  vec.erase();
+}
